e45c920986829db8a83f78dac6d25ba6b1e5b81e
Manas Pasayat <andolasoft.user187@gmail.com>
2023-12-20 18:23:28 +0530

    integrated mongodb atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
+const DB =
+  "mongodb+srv://manaspasayat3:ov9VDRUAJaQmlWWs@cluster0.ublqajy.mongodb.net/subsidyX_api?retryWrites=true&w=majority";
+
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect("mongodb://localhost:27017/subsidyX_api_nodejs", {})
+  // .connect("mongodb://localhost:27017/subsidyX_api_nodejs", {})
+  .connect(DB, {})
   .then(() => console.log("Database connection successful"))
   .catch(() => console.log("Database connection faild"));
--- swagger.js
+++ swagger.js
@@ -2,7 +2,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Node js api project for mongodb",
+      title: "Node js api project for SubsidyX",
       version: "1.0.0",
     },
     servers: [
